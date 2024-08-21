2bb85d57799eef56a5743078b45e05ea000af6d0
odii-vivek <odedravivek.10@gmail.com>
2024-08-21 09:27:40 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+
+dotenv.config(); // Load environment variables
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // For parsing JSON bodies
+app.use(cors()); // Enable CORS for cross-origin requests
+
+// MongoDB Connection
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Routes
+// app.use('/api/auth', authRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
